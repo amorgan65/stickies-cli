8056ea6642c3d5cf9b12ebacba81c4bbe3b88a67
Alec Morgan <alec.morgan@berkeley.edu>
2023-08-16 19:29:52 -0700

    Added structure for parsing command line arguments given when ran

--- main.py
+++ main.py
@@ -11,26 +11,40 @@ import sys
 
 num_args = (len(sys.argv) - 1)) #number elements excluding name of program
 
+function_name = sys.argv[1]
 
-
-
-
-
-
-
+if function_name == 'new':
+    new_note = sys.argv[2:]
+    create_sticky(new_note, 'white')
+    #TODO create new note if arguments are correct (sanitize?), also need to assign a note ID to it
+elif function_name == 'delete':
+    noteID = sys.argv[2]
+    delete_sticky(noteID)
+    #TODO retrieve the associated note based on the typed in noteID stored in noteID, and delete it
+elif function_name == 'print':
+    print_notes()
+    #TODO if no more text after print, then print out all notes
+    # however, if we give a noteID, print out the note that's associated with it
+    # if a note doesn't exist, print out Error: note with that ID does not exist
+elif function_name == 'update':
+    noteID = sys.argv[2]
+    updated_text = sys.argv[3:]
+    update_sticky(noteID, updated_text)
+else:
+    #TODO print out how to use this program, correct syntax for using?
 
 
 class Note:
-    def __init__(self, color, content):
-        self.color = color
+    def __init__(self, content, sticky_color):
+        self.sticky_color = sticky_color
         self.content = content
-        
+        #TODO give this note an identifying note ID
         #save the sticky notes ID here?
 
         # save this note in some data structure (hash map??)
 
 
-def create_sticky(text):
+def create_sticky(text, color):
     #create square with |_-?
     #   _______________
     #  |1              |
@@ -38,6 +52,8 @@ def create_sticky(text):
     #  |     NOTE      |
     #  |               |
     #  |_______________|
+    new_sticky = Note(text, color)
+
 
 # Delete sticky note associated with a given ID (shown in top left of note)
 def delete_sticky(sticky_id):
